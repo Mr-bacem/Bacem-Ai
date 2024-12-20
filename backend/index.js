62ad0bd72575d165eebef5881a0cc93d5d632d3d
bacemgdr <bacem.ghdiri@gmail.com>
2024-12-20 23:39:53 +0100

    upload version

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: ['https://bacemai.netlify.app', 'https://bacemai.netlify.app'],
     credentials: true,
   })
 );
